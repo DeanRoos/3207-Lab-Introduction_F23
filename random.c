7ad2f705e82dd97b5b23671bbf3f78d03b4a566d
DeanRoos <tug91912@temple.edu>
2023-09-01 00:08:04 -0400

    Final push

--- random.c
+++ random.c
@@ -1,1 +1,11 @@
-test
+#include "random.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char randchar(){
+
+  // rang()/26 will generate an int between 0 and 25, and adding that to 'A' will give us each of the 26 upper case letters of the alphabet
+  return 'A' + (rand()%26);
+
+}
---
+++ random.h
@@ -0,0 +1,7 @@
+#ifndef random_h
+#define random_h
+
+//declaring randchar function
+char randchar();
+
+#endif
